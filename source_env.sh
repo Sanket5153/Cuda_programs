b34cdee32294e90dc68689a0b3b518f0d84963f7
Mr. Sanket <118500008+Sanket5153@users.noreply.github.com>
2024-12-30 08:40:31 +0530

    Create source_env.sh

---
+++ source_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Change this script according to own envrionment
+
+#Setup of SPACK Environment
+source /home/apps/spack/share/spack/setup-env.sh
+
+#Setup of CUDA Environment
+spack load cuda/t34vp3s
+
